1489b3da492b56d953016b883b3a05858231178d
Kronos062 <cristiaan0662@gmail.com>
2023-11-28 12:48:39 +0100

    CAAMBIADO de 50 a 5

--- hello_world.py
+++ hello_world.py
@@ -3,5 +3,5 @@
 
 # A esa primera linea se le llama shebang, indica como ejecutar codigo sin necesidad de indicar el programa que lo interpreta
 
-for i in range(0, 50):
+for i in range(0, 5):
     print("NO copiare en clase de programación")
