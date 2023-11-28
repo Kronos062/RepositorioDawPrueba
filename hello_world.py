ca836975c73e44179dc5696ceec525017e2f3853
Kronos062 <cristiaan0662@gmail.com>
2023-11-28 12:17:41 +0100

    Hemos añadido un ejecutable

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+# A esa primera linea se le llama shebang, indica como ejecutar codigo sin necesidad de indicar el programa que lo interpreta
+
+for i in range(0, 50):
+    print("NO copiare en clase de programación")
