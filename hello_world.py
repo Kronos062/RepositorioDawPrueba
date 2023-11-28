6cc64a5ed9a764d3c64f28dd4f43d2aaf0bdaa32
Kronos062 <cristiaan0662@gmail.com>
2023-11-28 12:46:01 +0100

    Hemos cambiado el texto

--- hello_world.py
+++ hello_world.py
@@ -4,4 +4,4 @@
 # A esa primera linea se le llama shebang, indica como ejecutar codigo sin necesidad de indicar el programa que lo interpreta
 
 for i in range(0, 10):
-    print("NO copiare en clase de programación")
+    print("Estaré atento a las explicaciones para no preguntar lo mismo 20 veces")
