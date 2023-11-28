07b208a1c25e8291c815cdeb0be548e86507ace9
Kronos062 <cristiaan0662@gmail.com>
2023-11-28 13:26:35 +0100

    gay1.0

--- hello_world.py
+++ hello_world.py
@@ -2,6 +2,7 @@
 
 
 # A esa primera linea se le llama shebang, indica como ejecutar codigo sin necesidad de indicar el programa que lo interpreta
+
 import random
 
 def calcular_porcentaje_aleatorio(minimo, maximo):
@@ -11,7 +12,13 @@ def main():
     minimo = 1
     maximo = 100
     porcentaje_aleatorio = calcular_porcentaje_aleatorio(minimo, maximo)
-    print(f"Eres {porcentaje_aleatorio}% puto")
+
+    if porcentaje_aleatorio < 50:
+        return "eres poco gay"
+    elif porcentaje_aleatorio > 50:
+        return "eres muy gay"
+    else:
+        return "eres medio gay"
 
 if __name__ == "__main__":
-    main()
+    print(main())
