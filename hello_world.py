1d2fd5f38d41a1ac7f8d9a47f6f7cf68a23403ff
Kronos062 <cristiaan0662@gmail.com>
2023-11-28 13:41:42 +0100

    mygrandma

--- hello_world.py
+++ hello_world.py
@@ -4,4 +4,4 @@
 # A esa primera linea se le llama shebang, indica como ejecutar codigo sin necesidad de indicar el programa que lo interpreta
 
 for i in range(0, 33):
-    print("Estaré atento a las explicaciones para no preguntar lo mismo 20 veces")
+    print("My grandma is 82 years old")
