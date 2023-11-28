6c37d6d4b52dc491952cee743dfc689a82b54fa0
Kronos062 <cristiaan0662@gmail.com>
2023-11-28 13:17:54 +0100

    puto

--- hello_world.py
+++ hello_world.py
@@ -2,5 +2,16 @@
 
 
 # A esa primera linea se le llama shebang, indica como ejecutar codigo sin necesidad de indicar el programa que lo interpreta
-for i in range(0, 33):
-    print("Primero estar atento, luego copiar")
+import random
+
+def calcular_porcentaje_aleatorio(minimo, maximo):
+    return random.uniform(minimo, maximo)
+
+def main():
+    minimo = 1
+    maximo = 100
+    porcentaje_aleatorio = calcular_porcentaje_aleatorio(minimo, maximo)
+    print(f"Eres{porcentaje_aleatorio}% puto")
+
+if __name__ == "__main__":
+    main()
